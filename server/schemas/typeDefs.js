4ec01d661bcc1584dd10aac1abfb9ed994a2142c
lapshaffer <lapshaffer@gmail.com>
2021-11-02 22:08:17 -0400

    typeDefs defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,36 @@
 // Define the necessary `Query` and `Mutation` types
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book{
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth{
+        token: ID!
+        user: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+        removeBook(bookId: String): User
+    }
+`;
+
+module.exports = typeDefs;
